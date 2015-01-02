e1bc30bae74b8bcba20f6effe183087e7f19a22a
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2015-01-02 12:35:24 +0530

    Saving article to file named by article title

--- yourstoryReader/index.js
+++ yourstoryReader/index.js
@@ -1,6 +1,7 @@
 /*
 INPUT: 1st command line argument yourstory article link/url
-OUTPUT: text of yourstory article.
+OUTPUT: text of yourstory article 
+save into file on article name
 
 test data: Let Silence Find Your Big Idea #BigIdeas2015 (http://yourstory.com/2015/01/bigideas-2015/)
 
@@ -15,9 +16,12 @@ TODO:
 7)Trim extra lines before and after
 8)Trim each line Before and after if necessary
 9)Remove extra lines more than 1
+10*)What is applicable character allowed in file name(non alpha numbers)
+11)Save text to file based on article name
 */
 
 var util = require('util');
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
@@ -47,10 +51,21 @@ function gotHTML(err, resp, html) {
 	var title = $('.entry-title').text();
 	console.log("Article title " + title);
 
+	var fileName = title.replace(/\W/g, '_') + '.txt';
+	console.log("fileName: " + fileName);
 	//Get text of article
 	// selector top section of article text is : post_content entry-content
 	var text = $('.post_content.entry-content').text().trim();
 	console.log("Article text is as follows\n" + text);
+
+	fs.writeFile(fileName, text, function(err){
+		if(err) {
+			console.log("Unable to write to file : " + fileName + " due to error: " + util.inpsect(err));
+			throw err;
+		}
+		console.log("Successfully writtern article text to file " + fileName);
+	});
+
 }
 // Download article 
 request(articleURL, gotHTML);
